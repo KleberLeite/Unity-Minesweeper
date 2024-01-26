b2fb60f72f24b419efbd6ac9d98d15f6e2baa135
kleberleite <kleberleitetavares@gmail.com>
2024-01-26 18:21:57 -0300

    feat: added Database.GetAll

--- Assets/Scriptables/Scripts/Databases/Database.cs
+++ Assets/Scriptables/Scripts/Databases/Database.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Minesweeper.Databases
@@ -30,5 +28,7 @@ namespace Minesweeper.Databases
 
             return datas[index];
         }
+
+        public Data[] GetAll() => datas;
     }
 }
